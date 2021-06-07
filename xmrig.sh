fcf9caddbe679046a24bbd25c83d74acd6360608
gerhanaku <73520690+gerhanaku@users.noreply.github.com>
2021-06-07 16:19:57 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,2 +1,2 @@
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
-./xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u TRX:TMskdroztKb8CWC38ZEg8sww2sm3v8sqTW.mamat
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TMskdroztKb8CWC38ZEg8sww2sm3v8sqTW.mamat
